a508d681662c4c72a394569caff2aca2bc7a1b45
Dr. Monaco F. J. <monaco@usp.br>
2026-09-04 18:34:42 -0300

    refactor(plotting): remove transitional plotting module

--- misda/_plotting_newapi.py
+++
@@ -1,8 +0,0 @@
-# SPDX-FileCopyrightText: 2025 Monaco F. J. <monaco@usp.br>
-# SPDX-License-Identifier: GPL-3.0-or-later
-
-"""Internal compatibility bridge to the canonical plotting module."""
-
-from ._plotting import plot_mis_set_graph
-
-__all__ = ["plot_mis_set_graph"]
